16252008827a2a551769a93f5ad29a730d396bba
girisimci <saitisik63@gmail.com>
2020-04-08 04:11:35 +0300

    admob Added :)

--- app/src/main/java/com/example/bedenimne/MainActivity.kt
+++ app/src/main/java/com/example/bedenimne/MainActivity.kt
@@ -6,14 +6,25 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import androidx.viewpager.widget.ViewPager
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 import com.google.android.material.tabs.TabLayout
 
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var mAdView: AdView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        MobileAds.initialize(this) { "ca-app-pub-4583834812275695~1580063729" }
+        mAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
 
         val tab = findViewById<TabLayout>(R.id.tab)
 
