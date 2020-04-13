bce411a6c65ed1fb16f4b49bb02f0f0af92e037d
girisimci <saitisik63@gmail.com>
2020-04-13 08:42:16 +0300

    Şapka Ölçüleri ve tasarımları tamamlandı

--- app/src/main/java/com/example/bedenimne/home.kt
+++ app/src/main/java/com/example/bedenimne/home.kt
@@ -8,39 +8,54 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.fragment_home.*
 
 
 class home : Fragment() {
 
-    private var olcusu: EditText? = null
-    private var btn: Button? = null
-    private var cevap: TextView? = null
+    private lateinit var olcusu: EditText
+    private lateinit var btn: Button
+    private lateinit var cevap: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        inflater.inflate(R.layout.fragment_home, container, false)
 
-        hesap()
-
-    }
-
-    private fun hesap() {
-
-        var olcusu = tv_kafa_olcu.text as EditText
-        var btn = btn_kafa_sonuc as Button
-        var cevap = tv_kafa_cevap.text as TextView
+        var view = inflater.inflate(R.layout.fragment_home, container, false)
+        olcusu = view.findViewById(R.id.tv_kafa_olcu)
+        btn = view.findViewById(R.id.btn_kafa_sonuc)
+        cevap = view.findViewById(R.id.tv_kafa_cevap)
 
         btn.setOnClickListener {
-            val n1 = olcusu.toString().toInt()
 
-            if (n1 == 54 || n1 == 53) {
-                cevap.text = "Xs bedene sahipsiniz."
+
+            var girdi: Int = olcusu.text.toString().toInt()
+            cevap.text.toString()
+
+
+            when (girdi) {
+                53 -> cevap.text = "Şapka Bedeniniz XS'dir"
+                54 -> cevap.text = "Şapka Bedeniniz XS'dir"
+                55 -> cevap.text = "Şapka Bedeniniz S'dir"
+                56 -> cevap.text = "Şapka Bedeniniz S'dir"
+                57 -> cevap.text = "Şapka Bedeniniz M'dir"
+                58 -> cevap.text = "Şapka Bedeniniz M'dir"
+                59 -> cevap.text = "Şapka Bedeniniz L'dir"
+                60 -> cevap.text = "Şapka Bedeniniz L'dir"
+                61 -> cevap.text = "Şapka Bedeniniz XL'dir"
+                62 -> cevap.text = "Şapka Bedeniniz XL'dir"
+
+                else -> cevap.text = "Şapka Bedeniniz Bilinmiyor!"
             }
+
         }
-    }
+
+
+        return view
+        }
 
 }
+
+
